3b5aeb85abbcd2d6e02c879412c835407cc29efb
Mohammed Ashir V <mohammedashir156@gmail.com>
2026-08-31 02:08:51 -0700

    Add files via upload

--- square.py
+++
@@ -1,8 +0,0 @@
-number_list=[1,2,3,4,5,6,7,8,9,10]
-print(f"original list: {number_list}")
-sq_events=[]
-for num in number_list:
-    if num%2==0:
-        sq_value=num**2
-        sq_events.append(sq_value)
-print(f"filtered and squared events: {sq_events}")
